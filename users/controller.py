6f3df48ea3a21ba5b61439031a5a8cfae7ca1bfe
Kullai Metikala <99985908+kullaisec@users.noreply.github.com>
2026-02-05 21:19:10 +0530

    Update controller.py

--- users/controller.py
+++ users/controller.py
@@ -1,1 +1,6 @@
+from users.service import get_user_by_email
+from flask import request, jsonify
 
+def get_user():
+    email = request.args.get("email")
+    return jsonify(get_user_by_email(email))
